af9fb480cf7b1c64c7705cb821d992ab96716a86
Julian Kuroiwa <julian.kuroiwa@gmail.com>
2020-04-07 14:40:29 -0300

    Created delete route.

--- src/app.js
+++ src/app.js
@@ -51,7 +51,16 @@ app.put("/repositories/:id", (request, response) => {
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  //TODO
+  const { id } = request.params;
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if(repositoryIndex < 0) {
+    return response.status(400).json({error: "ID not found"});
+  }
+
+  repositories.splice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
